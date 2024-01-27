8715d98aac5e7b7684730d9e00b2e15902b8deac
sayande717 <sayande717@github.com>
2024-01-27 17:13:14 +0530

    Computer Networks

--- push.sh
+++ push.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+read -p 'Subject ID (1-6): ' subjectID
 
+subjectName=("Mathematics"\
+    "Database Management"\
+    "Operating Systems"\
+    "Digital Logic"\
+    "Algorithms"\
+    "Computer Networks")
+
+git add *
+git commit -m "${subjectName[$subjectID-1]}"
+git push origin main
