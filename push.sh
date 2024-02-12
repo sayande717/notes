c7f4ec5cae49277dcdf43828140269927e575f94
sayande717 <sayande717@github.com>
2024-02-12 15:48:43 +0530

    fixed push.sh

--- push.sh
+++ push.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 read -p 'Subject ID (1-7): ' subjectID
 
-subjectName=("Discreet-Mathematics"\
-    "Database-Management"\
-    "Operating-Systems"\
-    "Digital-Logic"\
-    "Algorithms"\
+subjectName=("Discreet-Mathematics" \
+    "Database-Systems" \
+    "Operating-Systems" \
+    "Digital-Logic" \
+    "Algorithms" \
     "Computer-Networks" \
     "Aptitude" \
     "TIL-Coding")
 
 targetSub=${subjectName[$subjectID-1]}
+
 git add README.md ./topics/$targetSub.md ./assets/images/$targetSub/
 git commit -m "$targetSub"
 git push origin main
